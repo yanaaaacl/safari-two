7e6a37181c26e9afe199fc228df0c39fa80420b8
Yana Chachovskaya <a9506862137@yandex.ru>
2022-10-13 14:34:43 +0300

    gallery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 $(function(){
+    $('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
 
+		fixedContentPos: false
+	});
+
+	$('.gallery__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt="pictures"></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt="pictures"></button>'
+	});
+
+	$('.gallery__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		
+	});
 });
